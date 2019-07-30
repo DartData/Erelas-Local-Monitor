8338f847388b2049f7cd38dde10ae04e7c6ed88b
Tim <tim.mockford@gmail.com>
2019-07-30 08:15:45 +0100

    Changed direction - going for simple and working

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
-from pystray import MenuItem as item
-import pystray
-from PIL import Image
-import settings
+from time import sleep
+import paho.mqtt.publish as publish
+
+server = "192.168.0.1"
+channel = "Test"
+path = "C:\Test"
+filemon = 0
+lifemon = 1
+url = "https://www.google.com"
+urlmon =  0
 
 
-#set default variables
-global run
-global start_stop_label
-run = True
-start_stop_label = 'Stop'
+def readfile(): #read saved settings
+    global fileread
+    global server
+    global path
+    global filemon
+    global lifemon
+    global url
+    global urlmon
+    global channel
 
+    file = open("values.txt", "r")
+    fileread = file.read().splitlines()
+    server = fileread[0]
+    path = fileread[1]
+    filemon = fileread[2]
+    lifemon = fileread[3]
+    url = fileread[4]
+    urlmon = fileread[5]
+    channel = fileread[6]
+    file.close()
 
+def file_monitor():
+    if filemon == 1:
+        print("stuff")
+    elif filemon == 0:
+        sleep(0.1)
 
-def quit():
-    print("quit")
-    exit()
+def life_monitor():
+    if lifemon == 1:
+        print("stuff")
+    elif lifemon == 0:
+        sleep(0.1)
 
-def settings():
-    print("settings")
-    settings.main()
+def url_mon():
+    if urlmon == 1:
+        print("stuff")
+    elif urlmon == 0:
+        sleep(0.1)
 
-def process():
-    global run
-    if run == True:
-        print("Stopped")
-        run = False
-    elif run == False:
-        print("Started")
-        run = True
-    icon.update_menu()
+def write_log():
+    print("Stuff")
+
+def mqtt_post():
+    print("Stuff")
+    MQTT_SERVER = server
+    MQTT_PATH = channel
+    publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
 
 #setup icon in system tray
-image = Image.open("icon.png")
-menu = (item('Process', process, checked=lambda item:run), item('Settings', settings), item('Quit', quit))
-icon = pystray.Icon("name", image,"title",menu)
-
-#run the program
-icon.run()
+mqtt_post()
