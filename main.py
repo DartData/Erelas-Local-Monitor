b6f2bd8ad16b45dd930fe2143e3836440e168d3c
Tim <tim.mockford@gmail.com>
2019-07-30 22:59:13 +0100

    More sections completed
    Only the files section to perfect code for. Need to add post to url

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from time import sleep
-import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import psutil
+import urllib3
+import glob
+import os
 
-server = "192.168.0.1"
-channel = "Test"
-path = "C:\Test"
+#variables for mqtt and making intial connection
+broker_url = "192.168.1.108"
+broker_port = 1883
+
+client = mqtt.Client()
+client.connect(broker_url, broker_port)
+
+#placeholders
+mtopic = "helloworld"
+mpayload = "100"
+
+#variables per PC
+channel = r"hello/world"
+path = r"C:\Users\timmo\Downloads\*" # * indicates all file types will be scanned
 filemon = 0
 lifemon = 1
 url = "https://www.google.com"
 urlmon =  0
+computer = "DAD1"
 
-
-def readfile(): #read saved settings
+def readfile(): #read saved settings currently not used
     global fileread
     global server
     global path
@@ -31,32 +46,94 @@ def readfile(): #read saved settings
     channel = fileread[6]
     file.close()
 
-def file_monitor():
+def file_monitor(): #check logger for files
     if filemon == 1:
-        print("stuff")
+        list_of_files = glob.glob(path) 
+        latest_file = max(list_of_files, key=os.path.getctime)
+        print(latest_file)
+        
+
     elif filemon == 0:
         sleep(0.1)
 
-def life_monitor():
+def life_monitor(): #read CPU and RAM
     if lifemon == 1:
-        print("stuff")
+        #get CPU load
+        area = "cpu"
+        CPULOAD = psutil.cpu_percent(interval=1, percpu=False)
+        CPULOAD = float(CPULOAD)
+        print(CPULOAD)
+
+        mtopic = (computer + "-" + area)
+        print(mtopic)
+        CPULOAD = str(CPULOAD)
+        mqtt_post(mtopic,CPULOAD)
+
+        #get memory load
+        area = "mem"
+        MEMLOAD = psutil.virtual_memory().percent
+        MEMLOAD = float(MEMLOAD)
+        print(MEMLOAD)
+
+        mtopic = (computer + "-" + area)
+        print(mtopic)
+        MEMLOAD = str(MEMLOAD)
+        mqtt_post(mtopic,MEMLOAD)
+
+        #get disk usage
+        area = "disk"
+        DISKUSE = psutil.disk_usage('/').percent
+        DISKUSE = float(DISKUSE)
+        print(DISKUSE)
+
+        mtopic = (computer + "-" + area)
+        print(mtopic)
+        DISKUSE = str(DISKUSE)
+        mqtt_post(mtopic,DISKUSE)
+
     elif lifemon == 0:
         sleep(0.1)
 
-def url_mon():
+def url_mon(): #check url for existance
     if urlmon == 1:
-        print("stuff")
+        #controls url requests
+        http = urllib3.PoolManager()
+
+        #check for http code and get result
+        check = http.request('GET', 'http://stream.crossrhythms.co.uk/plymouth/hq.mp3',preload_content=False)
+        CHECKRESULT = check.status
+        print(CHECKRESULT)
+
+        #determine fields to send
+        if CHECKRESULT == 200:
+            STATUS = "GREEN"
+            VALUE = CHECKRESULT
+            MEASURE = "HTTP CODE"
+        else:
+            STATUS = "RED"
+            VALUE = CHECKRESULT
+            MEASURE = "HTTP CODE"
+
+        #check
+        print(STATUS)
+        print(VALUE)
+        print(MEASURE)
+
+        #transmit result
+        area = "url"
+        mtopic = (computer + "-" + area)
+        print(mtopic)
+        mqtt_post(mtopic,STATUS)
+
     elif urlmon == 0:
         sleep(0.1)
 
-def write_log():
+def write_log(): #write local log of errors / start ups
     print("Stuff")
 
-def mqtt_post():
-    print("Stuff")
-    MQTT_SERVER = server
-    MQTT_PATH = channel
-    publish.single(MQTT_PATH, "Hello World!", hostname=MQTT_SERVER)
+def mqtt_post(mtopic,mpayload): #send to mqtt server
+    print("Posting message from " + mtopic)
+    client.publish(topic=mtopic, payload=mpayload, qos=2, retain=False)
 
 #setup icon in system tray
-mqtt_post()
+life_monitor()
